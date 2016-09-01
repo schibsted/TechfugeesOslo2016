f0fc019c0769ec67801c3ef7408938e76674bebc
Jesper Thøgersen <jesper@thog.no>
2016-09-01 23:54:07 +0200

    styles sourcemaps & autoprefixer

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,19 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 const webpack = require('gulp-webpack');
 const ghPages = require('gulp-gh-pages');
 
 gulp.task('styles', () => {
+    const processors = [require('autoprefixer')];
+
     return gulp.src('_assets/styles/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('css'));
+        .pipe(postcss(processors))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.styles.dest))
 });
 
 gulp.task('webpack', () => {
