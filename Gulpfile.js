ab9eac2c4865c7d2353d054506b158588451a8ed
Jesper Thøgersen <jesper@thog.no>
2016-09-01 22:28:34 +0200

    assets

---
+++ Gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const webpack = require('gulp-webpack');
+const ghPages = require('gulp-gh-pages');
+
+gulp.task('styles', () => {
+    return gulp.src('_assets/styles/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('build/styles'));
+});
+
+gulp.task('deploy', () => {
+    return gulp.src('build/**/*')
+        .pipe(ghPages());
+});
