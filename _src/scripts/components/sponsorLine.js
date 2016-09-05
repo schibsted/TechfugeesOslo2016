69bbdd8b486810e889c11add16bd2511042f4abf
Jesper Thøgersen <jesper@thog.no>
2016-09-05 22:42:55 +0200

    shuffle sponsor logos

--- _src/scripts/components/sponsorLine.js
+++ _src/scripts/components/sponsorLine.js
@@ -1,5 +1,9 @@
 export default {
     init (root, refs) {
 
+        for (let i = refs.logos.children.length; i >= 0; i--) {
+            refs.logos.appendChild(refs.logos.children[Math.random() * i | 0]);
+        }
+
     }
 };
